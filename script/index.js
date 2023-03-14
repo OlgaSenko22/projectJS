a53ae19df36f043c641e76914c7ca3fd8a1ecae9
Olga <olga1998senko@gmail.com>
2023-03-14 13:07:37 +0300

    project8

--- script/index.js
+++ script/index.js
@@ -1,10 +1,25 @@
 "use strict";
 
-let num = 1;
-num+=12;
-num-=14;
-num*=5;
-num/=7;
-num++;
-num--;
-alert(num);
+//Task1
+const userQuestion = +prompt('In what year was the city of Kiev founded?');
+const result = userQuestion===482 ? 'True' : 'Go to study a history!';
+alert(result);
+
+
+//Task2
+const numOrStr = prompt('Input number to string');
+console.log(numOrStr);
+switch (numOrStr) {
+    case numOrStr === null:
+        console.log('вы отменили');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
