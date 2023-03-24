16cac98beb3ef164d25f52ee795ac681e3cc3436
Olga <olga1998senko@gmail.com>
2023-03-24 22:48:10 +0300

    project13

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 "use strict";
+const userNumber = +prompt('Enter number', '1');
+if(userNumber === 1 ) {
+    alert(`${userNumber} год`);
+} else if(userNumber>1 && userNumber<=4) {
+    alert(`${userNumber} года`);
+} else if(isNaN(userNumber)) {
+    alert('Нужно писать число');
+} else {
+    alert(`${userNumber} лет`);
+}
