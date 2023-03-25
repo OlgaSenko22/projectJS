7b86688457e9e1bc9476e68368b43a7e4a907efb
Olga <olga1998senko@gmail.com>
2023-03-26 00:03:11 +0300

    project12 change

--- script/index.js
+++ script/index.js
@@ -2,5 +2,6 @@
 const x = 4;
 const y = 9;
 const r = 10;
-const result = x**2 + y**2 <= r**2 ? 'given point inside the circle' : 'given point outside the circle';
+const z = Math.sqrt(x**2+y**2);
+const result = z <= r ? 'given point inside the circle' : 'given point outside the circle';
 alert(result);
