e5a1091fecf21b67eb881cdd0042a1bf32da9c65
Olga <olga1998senko@gmail.com>
2023-04-17 10:14:36 +0300

    project16

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 "use strict";
+const userNumber = +prompt('Enter number', '1');
+const userNumberPower = +prompt('Enter number power', '2');
+function foo (userNumber, userNumberPower) {
+    if(isNaN(userNumber) || isNaN(userNumberPower)) {
+        return 'some error';
+    } else {
+        return Math.pow(userNumber, userNumberPower);
+    }
+}
+alert(foo(userNumber, userNumberPower));
