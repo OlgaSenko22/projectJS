290531390d3f6589a9e46cfe8ffcec1e21273dd1
Olga <olga1998senko@gmail.com>
2023-04-17 10:18:34 +0300

    project16 changed

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 "use strict";
 const userNumber = +prompt('Enter number', '1');
-const userNumberPower = +prompt('Enter number power', '2');
+const userNumberPower = +prompt('Enter number power', '1');
 function foo (userNumber, userNumberPower) {
     if(isNaN(userNumber) || isNaN(userNumberPower)) {
         return 'some error';
