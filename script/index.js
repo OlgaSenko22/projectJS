9f6532fb032c0c5c0fb642d96e8155fa2936b672
Olga <olga1998senko@gmail.com>
2023-04-22 16:27:09 +0300

    project18

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
 "use strict";
+const arr = [1, 2, 3, -1, -2, -3];
+const foo = (arr) => {
+    const exampleArr = [];
+    if(!arr.length) return 'the array is empty';
+    for(let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if(num>0) exampleArr.push(num);
+    }
+    return exampleArr.length ? exampleArr : null;
+}
+const exampleArr = foo(arr);
+console.log(exampleArr);
