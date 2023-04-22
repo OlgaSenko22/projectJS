2751ee7a144306a1e2cff7484ce0ea77071a5d4f
Olga <olga1998senko@gmail.com>
2023-04-22 22:49:36 +0300

    project20

--- script/index.js
+++ script/index.js
@@ -1,1 +1,12 @@
 "use strict";
+const fib = (n = 1) => {
+    let f1 = 1;
+    let f2 = f1;
+    for( let i = 3; i<=n; i++) {
+        let F = f1 + f2;
+        f1 = f2;
+        f2 = F;
+    }
+    return f2;
+}
+alert(fib(77));
