5175b8c0f93230d2718287967a876fc3f81b6819
Olga <olga1998senko@gmail.com>
2023-04-22 20:20:16 +0300

    project19

--- script/index.js
+++ script/index.js
@@ -1,1 +1,10 @@
 "use strict";
+const weight = 75;
+const liter = 48;
+const price = 13;
+const potatoes = (weight, liter, price) => {
+    const num1 = weight * 4;
+    const num2 = (num1 * liter)/1000;
+    return Math.round(num2 * price);
+}
+console.log(potatoes(weight, liter, price));
