af0b2788cde0f66f57b9be6b2ca6a7d5ee02b811
Olga <olga1998senko@gmail.com>
2023-04-23 10:36:15 +0300

    project21

--- script/index.js
+++ script/index.js
@@ -1,1 +1,9 @@
 "use strict";
+const userNumber = +prompt('Enter number');
+const foo = (userNumber) => {
+    for(let i = 2; i<userNumber; i++) {
+        if(userNumber % i === 0) return 'false';
+    }
+    return userNumber > 1;
+}
+console.log(foo(userNumber));
