ecf1c25e89157a2962996ba3e27b1b40c916d877
Olga <olga1998senko@gmail.com>
2023-04-23 11:10:59 +0300

    project21 change

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@
 const userNumber = +prompt('Enter number');
 const foo = (userNumber) => {
     for(let i = 2; i<userNumber; i++) {
-        if(userNumber % i === 0) return 'false';
+        if(userNumber % i === 0) return false;
     }
     return userNumber > 1;
 }
