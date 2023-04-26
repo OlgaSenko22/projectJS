dc5552afe7843adc96cf8d30c443bf640c9a9edc
Olga <olga1998senko@gmail.com>
2023-04-26 11:04:27 +0300

    project36

--- script/index.js
+++ script/index.js
@@ -1,1 +1,23 @@
 "use strict";
+let calculator = {
+    read() {
+        this.a = prompt('Enter first number', '1');
+        while(isNaN(this.a) || this.a === null || this.a.trim() === '') {
+            this.a = prompt('Enter first number', '1');
+        }
+        this.b = prompt('Enter second number', '2');
+        while(isNaN(this.b) || this.b === null || this.a.trim() === '') {
+            this.b = prompt('Enter second number', '2');
+        }
+    },
+    sum() {
+        return Number(this.a) + Number(this.b);
+    },
+    mul() {
+        return Number(this.a) * Number(this.b);
+    }
+};
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
