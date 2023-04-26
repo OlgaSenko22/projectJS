b622603159ca82459b51f3d3c8ce3232ca078084
Olga <olga1998senko@gmail.com>
2023-04-26 12:04:14 +0300

    project38

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
 "use strict";
+let ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert( this.step );
+    }
+};
+ladder.up().up().down().showStep();
