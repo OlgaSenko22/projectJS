2f992369060ec72b36e663e324933579379e9bc4
Olga <olga1998senko@gmail.com>
2023-05-01 21:59:05 +0300

    project33

--- script/index.js
+++ script/index.js
@@ -1,1 +1,12 @@
 "use strict";
+function foo () {
+    let num = 0;
+    return function (y) {
+    num += y;
+    return num;
+    }
+}
+let myFunc = foo();
+console.log(myFunc(3));
+console.log(myFunc(5));
+console.log(myFunc(20));
