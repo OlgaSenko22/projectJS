30fa7c4bc0ae0bd058a5a46b74ba03b079f427c5
Olga <olga1998senko@gmail.com>
2023-05-02 21:14:55 +0300

    project25

--- script/index.js
+++ script/index.js
@@ -1,1 +1,11 @@
 "use strict";
+//Task1
+const arr1 = ['a', 'b', 'c', 'd'];
+const newArr1 = `${arr1[0]}+${arr1[1]}, ${arr1[2]}+${arr1[3]}`
+console.log(newArr1);
+//Task2
+const arr2 = [2, 5, 3, 9];
+
+
+
+
