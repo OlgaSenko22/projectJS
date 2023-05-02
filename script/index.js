e6e9e9168e9dac2fdde9d721823541c9b3ca952b
Olga <olga1998senko@gmail.com>
2023-05-02 22:14:01 +0300

    project32

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
 "use strict";
+function func(arr) {
+    if(arguments.length > 1) {
+        throw new Error('Function accepts only 1 argument, too much arguments provided');
+    }
+    const result = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            result.concat(func(arr[i]));
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+console.log(func(5));
