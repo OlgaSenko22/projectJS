5d0a7506c8fbca86174b31352d523c8297947ed8
Olga <olga1998senko@gmail.com>
2023-05-02 21:34:23 +0300

    project22

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
 "use strict";
+const userNumber = +prompt('Enter number');
+if(isNaN(userNumber)) {
+    console.log('NaN');
+} else if(userNumber<2) {
+    console.log('the number must be greater than or equal to 2');
+} else {
+    let num = 2
+    while(userNumber % num !==0) {
+    num++;
+}
+console.log(num);
+}
