247787bdc1e7cc7720d8687a5fed8504543f176c
Olga <olga1998senko@gmail.com>
2023-05-02 23:03:37 +0300

    project23

--- script/index.js
+++ script/index.js
@@ -1,1 +1,26 @@
 "use strict";
+const mainFunction = (callback) => {
+    const userNumber = +prompt('Enter number');
+    const userPower = +prompt('Enter power number');
+    callback(userNumber, userPower);
+}
+const exponentiation = (userNumber, userPower) => {
+    const result = Math.pow(userNumber, userPower);
+    alert(result);
+}
+mainFunction(exponentiation);
+const multiplay = (userNumber, userPower) => {
+    const result = userNumber * userPower;
+    alert(result);
+}
+mainFunction(multiplay);
+const division = (userNumber, userPower) => {
+    const result = userNumber / userPower;
+    alert(result);
+}
+mainFunction(division);
+const modulo = (userNumber, userPower) => {
+    const result = userNumber % userPower;
+    alert(result);
+}
+mainFunction(modulo);
