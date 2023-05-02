d0c223494ba3d6d6f1d006d798319bd3593243e4
Olga <olga1998senko@gmail.com>
2023-05-02 14:44:00 +0300

    project34

--- script/index.js
+++ script/index.js
@@ -1,1 +1,19 @@
 "use strict";
+const foo = (length = 100) => {
+    const arr = [];
+    const numRandom = () => {
+        if (arr.length === length) return NaN;
+        const num = Math.floor((Math.random() * length) + 1);
+        if(arr.includes(num)) return numRandom();
+        arr.push(num);
+        return num;
+    }
+    return numRandom;
+}
+const numGeneration = foo();
+let func = true;
+while(func) {
+    const getNum = numGeneration();
+    if(isNaN(getNum)) func = false;
+    console.log(getNum)
+}
