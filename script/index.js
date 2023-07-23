bddafb2ff1b16dafc6f2936c2a0d41afdd2e8a29
Olga <olga1998senko@gmail.com>
2023-07-23 20:39:18 +0300

    homework67

--- script/index.js
+++ script/index.js
@@ -1,1 +1,21 @@
 "use strict";
+const goods = [['футболка', 15], ['шорти', 25], ['носки', 5]];
+function totalCost (goods) {
+    const searchGoods = new Map();
+    goods.forEach(([good, price]) => {
+        searchGoods.set(good, price)
+    })
+    function sumPrice(map) {
+        let sum = 0;
+        for(let key of map.values()){
+            sum +=key;
+        }
+        return sum;
+    }
+    const total = sumPrice(searchGoods);
+    return total;
+}
+
+console.log(totalCost(goods));
+
+
