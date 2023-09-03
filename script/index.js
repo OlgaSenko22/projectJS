fc74570dc3533a33d5c22616dd697b32b47add77
Olga <olga1998senko@gmail.com>
2023-09-03 21:37:21 +0300

    homework68

--- script/index.js
+++ script/index.js
@@ -1,1 +1,10 @@
 "use strict";
+const string = "abracadabra";
+function numberUnique(String) {
+    const stringSet = new Set();
+    for(const every of String){
+        stringSet.add(every)
+    }
+    return stringSet.size
+}
+console.log(numberUnique(string));
